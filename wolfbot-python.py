720ffca01c9da485bf695872ec163fd7deb72ba7
Wolf Hack <39095919+evilpeer@users.noreply.github.com>
2019-09-17 10:34:46 -0400

    removido de aqui el clasificador bayesiano
    Todas las líneas referentes al clasificador bayesiano ingenuo fueron removidas de aquí para pasar a un archivo independiente

--- wolfbot-python.py
+++ wolfbot-python.py
@@ -49,96 +49,9 @@ def cargar_datos(symbol):
          candle.append(temporal) # coño, al fin un solo arreglo con toda la data!
    total = total - 1
 
-##### injerto proveniente de naive_bayes.py
-
-def loadCsv(filename):
-	lines = csv.reader(open(filename, "r"))
-	dataset = list(lines)
-	for i in range(len(dataset)):
-		dataset[i] = [float(x) for x in dataset[i]]
-	return dataset
-
-def splitDataset(dataset, splitRatio):
-#	trainSize = int(len(dataset) * splitRatio)
-	trainSize = int(len(dataset) - 5)
-	trainSet = []
-	copy = list(dataset)
-	while len(trainSet) < trainSize:
-		index = random.randrange(len(copy))
-		trainSet.append(copy.pop(index))
-	return [trainSet, copy]
-
-def separateByClass(dataset):  #separo los que tienen (1) de los que no (0) en dos campos del list
-	separated = {}
-	for i in range(len(dataset)):
-		vector = dataset[i]
-		if (vector[-1] not in separated):
-			separated[vector[-1]] = []
-		separated[vector[-1]].append(vector)
-	return separated
-
-def mean(numbers):
-	return sum(numbers)/float(len(numbers))
-
-def stdev(numbers):
-	avg = mean(numbers)
-	variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
-	return math.sqrt(variance)
-
-def summarize(dataset):
-	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-	del summaries[-1]
-	return summaries
-
-def summarizeByClass(dataset):
-	separated = separateByClass(dataset)
-	summaries = {}
-	for classValue, instances in separated.items():
-		summaries[classValue] = summarize(instances)
-	return summaries
-
-def calculateProbability(x, mean, stdev):
-	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
-
-def calculateClassProbabilities(summaries, inputVector):
-	probabilities = {}
-	for classValue, classSummaries in summaries.items():
-		probabilities[classValue] = 1
-		for i in range(len(classSummaries)):
-			mean, stdev = classSummaries[i]
-			x = inputVector[i]
-			probabilities[classValue] *= calculateProbability(x, mean, stdev)
-	return probabilities
-			
-def predict(summaries, inputVector):
-	probabilities = calculateClassProbabilities(summaries, inputVector)
-	bestLabel, bestProb = None, -1
-	for classValue, probability in probabilities.items():
-		if bestLabel is None or probability > bestProb:
-			bestProb = probability
-			bestLabel = classValue
-	return bestLabel
-
-def getPredictions(summaries, testSet):
-	predictions = []
-	for i in range(len(testSet)):
-		result = predict(summaries, testSet[i])
-		predictions.append(result)
-	return predictions
-
-def getAccuracy(testSet, predictions):
-	correct = 0
-	for i in range(len(testSet)):
-		if testSet[i][-1] == predictions[i]:
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0
-
-
-
 ### main ###
 
-config = load_ini()                     # Leyendo el archivo de configuración y tomando los parametros correspondientes
+config = load_ini()                      # Leyendo el archivo de configuración y tomando los parametros correspondientes
 number_coins = int(config.get('exchange', 'pairs'))
 exchange = (config.get('exchange', 'name'))
 aviable_pairs = exchange+".pairs"
@@ -150,12 +63,11 @@ local_path = (config.get('path', 'local_path'))
 db_file = (config.get('path', 'db_path'))+(config.get('exchange', 'db_file'))
 db_name = (config.get('sql', 'db_name'))
 
-conn = sql_open(db_file)                # abro la base de  datos y consulto el ultimo registro
+conn = sql_open(db_file)                 # abro la base de  datos y consulto el ultimo registro
 dbh = conn.cursor()
-
-
-# Se puede hacer como bactrace pasando el ultimo timestamp a analizar y real-time pasando el ultimo timestamp guardado
-date_last = int(sys.argv[1])   # paso el ultimo valor del timestamp a analizar
+                                         # Se puede hacer como bactrace pasando el ultimo timestamp a analizar y real-time
+                                         # pasando el ultimo timestamp guardado
+date_last = int(sys.argv[1])             # paso el ultimo valor del timestamp a analizar
 
 date_first = date_last - 146700;         #un numero arbitrario por ahora
 cargar_datos(coin[0])
@@ -164,101 +76,12 @@ cargar_datos(coin[0])
 print(" timestamp    open    high     low   close    volume        vwp trades")
 print((str(candle[total][0]))+" "+(str(candle[total][1]))+" "+(str(candle[total][2]))+" "+(str(candle[total][3]))+" "+(str(candle[total][4]))+" "+(str(candle[total][5]))+" "+(str(candle[total][6]))+" "+(str(candle[total][7])))
 
-# ahora escribo el archivo que necesito para la grafica y los calculos que vienen 
+                                         # ahora escribo el archivo que necesito para la grafica y los calculos que vienen 
 bitmex_dat = open("./bitmex-5m.dat", "w")
 for n in range(0, total+1):
    linea = (str(candle[n][0])+","+str(candle[n][1])+","+str(candle[n][2])+","+str(candle[n][3])+","+str(candle[n][4])+","+str(candle[n][5])+","+str(candle[n][6])+","+str(candle[n][7]))
    bitmex_dat.write(str(linea) + '\n')
 bitmex_dat.close()
-
-
-#cochinon esta parte, solo me interesa que camine, la elegancia al carajo
-# timestamp, open, high, low, close, volume, vwp, trades 
-bitmex_pre = open("./bitmex-5m.csv", "w")
-for n in range(0, total-9):
-   linea = ""
-   temp =candle[n][1]-candle[n][6]  
-   linea = linea + str(temp)
-   temp =candle[n][2]-candle[n][6]  
-   linea = linea + "," + str(temp)
-   temp =candle[n][3]-candle[n][6]  
-   linea = linea + "," + str(temp)
-   temp =candle[n][4]-candle[n][6]  
-   linea = linea + "," + str(temp) + "," + str(candle[n][5]) + "," + str(candle[n][7])
-#   print("linea1 "+linea)
-
-#   linea = ""
-
-#   temp =candle[n][1]-candle[n][6]  
-#   linea = linea + str(temp)
-
-#   for o in range(2, 4):
-#      temp =candle[n][o]-candle[n][6]  
-#      linea = linea + "," + str(temp)
-
-#   linea = linea + "," + str(temp) + "," + str(candle[n][5]) + "," + str(candle[n][7])
-      
-#   print("linea2 " +linea)
-
-   for o in range(n, n+9):
-      if candle[o][4] > (candle[n][4]+((candle[n][4]*0.5)/100)):
-         flag = 1
-         break
-      if candle[o][4] < ((candle[n][4])-((candle[n][4]*0.5)/100)):
-         flag = -1
-         break
-      flag = 0
-   linea = linea + "," + (str(flag))
-
-   bitmex_pre.write(str(linea) + '\n')
-
-#print(linea)
-
-
-#preparo el tuple de imput para el sistema
-dataset = []
-inputtest = []
-linea = ""
-temp =candle[total][1]-candle[total][6] 
-dataset.append(temp)
-linea = linea + str(temp)
-temp =candle[total][2]-candle[total][6]  
-dataset.append(temp)
-linea = linea + "," + str(temp)
-temp =candle[total][3]-candle[total][6]  
-dataset.append(temp)
-linea = linea + "," + str(temp)
-temp =candle[total][4]-candle[total][6]  
-dataset.append(temp)
-linea = linea + "," + str(temp) + "," + str(candle[n][5]) + "," + str(candle[n][7])
-
-dataset.append(candle[total][5])
-dataset.append(candle[total][7])
-dataset.append(1)
-
-inputtest.append(dataset)
-#print(linea)
-#print(str(inputtest))
-
-bitmex_pre.close()
-
-
-##### injerto
-filename = 'bitmex-5m.csv'
-splitRatio = 0.67
-dataset = loadCsv(filename)
-trainingSet, testSet = splitDataset(dataset, splitRatio)
-#print(str(len(dataset) - len(trainingSet)))
-# prepare model
-summaries = summarizeByClass(trainingSet)
-# test model
-predictions = getPredictions(summaries, testSet)
-        
-accuracy = getAccuracy(testSet, predictions)
-
-#ejecuto prediccion
-predictions = getPredictions(summaries, inputtest)
-print("Prediction: " + str(predictions) + " Accuracy: " + str(accuracy) + "% tested:" + str(len(dataset) - len(trainingSet)) )
 exit()
 
 
